74bde3406116b0572ccb81d34272575f615649a5
jdromine <jromines@gmail.com>
2018-09-20 18:13:39 -0400

    THUM-241 - Adding message processing jobs

--- src/jobs/jobconfiguration.js
+++ src/jobs/jobconfiguration.js
@@ -1,19 +1,17 @@
 const JobTypes = require('./jobtypes.js');
-const Email = require('./email.js');
+const EmailProcessor = require('./emailProcessor.js');
+const PushNotificationProcessor = require('./pushNotificationProcessor.js');
 const Success = require('../eventhandlers/success.js');
 const Failure = require('../eventhandlers/failure.js');
 const Start = require('../eventhandlers/start.js');
 
 exports.defineJobs = function(agenda){
-    agenda.define(JobTypes.WELCOME_EMAIL, Email.welcomeEmail);
+  agenda.define(JobTypes.PUSH_NOTIFICATION_PROCESSOR, PushNotificationProcessor.run);
+  agenda.define(JobTypes.EMAIL_MESSAGE_PROCESSOR, EmailProcessor.run);
 }
 
 exports.setEvents = function(agenda){
-    agenda.on('start', Start.onStart);
-    agenda.on('success', Success.onSuccess);
-    agenda.on('failure', Failure.onFailure);
-}
-
-exports.scheduleRecurringJobs = function(agenda){
-
+  agenda.on('start', Start.onStart);
+  agenda.on('success', Success.onSuccess);
+  agenda.on('failure', Failure.onFailure);
 }
